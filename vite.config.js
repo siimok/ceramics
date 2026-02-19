1d2bec3bcaa60e9460b3a55dcfd4a7db03a417c8
Siimo <siimokangruoja@hotmail.com>
2026-02-19 21:34:42 +0200

    Fix: Change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,21 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-    base: '/ceramics/'
+    base: '/ceramics/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                galerii: resolve(__dirname, 'galerii.html'),
+                hinnakiri: resolve(__dirname, 'hinnakiri.html'),
+                kontakt: resolve(__dirname, 'kontakt.html'),
+                tooted: resolve(__dirname, 'tooted.html'),
+            },
+        },
+    },
+    assetsInclude: ['finished/*.jpg', 'product/**/*'],
 })
